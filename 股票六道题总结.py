c5665e8d773406f165d3f6b5ad76e2f170bc947b
HuangHuiBeyond <2215753082@qq.com>
2019-09-03 10:41:58 +0800

    股票六道题

--- 股票六道题总结.py
+++ 股票六道题总结.py
@@ -17,13 +17,13 @@ def max_profit_1(prices):
 
 
     ## 把空间复杂度降为1
-        if not prices:
-            return 0
-        size = len(prices)
-        dp_i_0 = 0 ## 是否留有股票（0：不留有，1：留有）
-        dp_i_1 = -prices[0]              ## 在buy的时候把交易次数减小1
-        for i in range(1, size):
-            
-                dp_i_0 = max(dp_i_0, dp_i_1 + prices[i])
-                dp_i_1 = max(dp_i_1,  0 - prices[i])
-        return dp_i_0
+    if not prices:
+        return 0
+    size = len(prices)
+    dp_i_0 = 0 ## 是否留有股票（0：不留有，1：留有）
+    dp_i_1 = -prices[0]              ## 在buy的时候把交易次数减小1
+    for i in range(1, size):
+        
+            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i])
+            dp_i_1 = max(dp_i_1,  0 - prices[i])
+    return dp_i_0

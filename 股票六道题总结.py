43203417c1e6e7cf528dace61537747fe9ae08ba
HuangHuiBeyond <2215753082@qq.com>
2019-09-03 10:30:31 +0800

    股票六道题总结

---
+++ 股票六道题总结.py
@@ -0,0 +1,16 @@
+## 用状态机来完成6道题目
+
+### 初始化一个三维度数组
+## 用状态机来完成
+def max_profit_1(prices):
+    ## 状态要学会变通，如果只能一次
+    if not prices:
+        return 0
+    size = len(prices)
+    dp = [[0, 0] for _ in range(size)] ## 是否留有股票（0：不留有，1：留有）
+    dp[0][1] = -prices[0]              ## 在buy的时候把交易次数减小1
+    for i in range(1, size):
+        
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+            dp[i][1] = max(dp[i - 1][1],  0 - prices[i])
+    return dp[size - 1][0]
